be6264fd8df652e4d3d65c4350f7c33645734645
Rogumann <lasse.pladsen@hotmail.com>
2023-06-25 01:19:28 +0200

    Implemented a way to change the download folder path.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import asyncio
+import json
+import os
+import sys
 import threading
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
 from pytube import YouTube, exceptions, Stream
 
-DOWNLOAD_PATH = r"G:\Users\Joachim\Downloads"
-WINDOW_WIDTH = 230
+
+def get_absolute_path(relative_path: str) -> str:
+    """Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+WINDOW_WIDTH = 281
 WINDOW_HEIGHT = 190
+CONFIG_JSON_PATH = get_absolute_path("data/config.json")
+FOLDER_IMAGE_PATH = get_absolute_path("data/folder.png")
+FOLDER_IMAGE_SIZE = 35, 35
+DOWNLOAD_FOLDER_TITLE = "Select a Download Directory"
+
 
 async def async_download_message() -> None:
     result_label.configure(text="Downloading...")
@@ -60,7 +74,7 @@ def get_stream(url: str, format_type: str, resolution: str) -> tuple[Stream | No
         outstring = "RegexMatchError getting the video stream."
         return None, outstring
 
-    if stream is None:   # no stream gotten
+    if stream is None:  # no stream gotten
         outstring = f"No stream found in {resolution}."
     else:
         stream.download(DOWNLOAD_PATH, filename=stream.default_filename.replace("mp4", format_type))
@@ -86,10 +100,56 @@ def on_format_select(*args) -> None:
         resolution_var.set("Max (w/ audio)")
     else:
         resolution_combo.configure(state="disabled")
-        resolution_var.set("")   # Clear the resolution selection
+        resolution_var.set("")  # Clear the resolution selection
+
+
+def file_exists(filename: str) -> bool:
+    """Checks if a file exists."""
+    try:
+        with open(filename, "r"):
+            pass
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def get_json_data(key: str, file: str = CONFIG_JSON_PATH) -> str:
+    """Extracts string value from given datakey from a given .json filename. Defaults to discord_data.json"""
+    if not file_exists(file):
+        change_download_folder()
+    with open(file, "r") as infile:
+        data = json.load(infile)
+    return data[key]
+
+
+def set_json_data(key: str, value: str, file: str = CONFIG_JSON_PATH) -> None:
+    """Sets a key and value in a given .json filename. Defaults to discord_data.json"""
+    with open(file, "r") as infile:
+        data = json.load(infile)
+    data[key] = value
+    with open(file, "w") as outfile:
+        json.dump(data, outfile, indent=3)
+
+
+def change_download_folder() -> None:
+    """Changes the download folder."""
+    filepath = filedialog.askdirectory(title=DOWNLOAD_FOLDER_TITLE)
+    if filepath:
+        set_json_data("download_path", filepath)
+
 
 root = tk.Tk()
+root.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 root.title("YouTube Downloader")
+
+# Check if download path is set, if not, ask for it
+if file_exists(CONFIG_JSON_PATH):
+    with open(CONFIG_JSON_PATH, "r") as f:
+        DOWNLOAD_PATH = get_json_data("download_path")
+else:
+    DOWNLOAD_PATH = filedialog.askdirectory(title=DOWNLOAD_FOLDER_TITLE)
+    with open(CONFIG_JSON_PATH, "w") as f:
+        json.dump({"download_path": DOWNLOAD_PATH}, f)
 
 # Styling
 style = ttk.Style().configure(
@@ -116,7 +176,7 @@ format_label.pack()
 
 format_var = tk.StringVar()
 format_var.set("mp4")
-format_combo = ttk.Combobox(root, textvariable=format_var, values=["mp4", "mp3"],state="readonly")
+format_combo = ttk.Combobox(root, textvariable=format_var, values=["mp4", "mp3"], state="readonly")
 format_combo.pack()
 
 format_var.trace("w", on_format_select)
@@ -128,7 +188,7 @@ resolution_label.pack()
 resolution_var = tk.StringVar()
 resolution_var.set("Max (w/ audio)")
 resolution_combo = ttk.Combobox(root, textvariable=resolution_var,
-                                values=["Max (w/ audio)","1080p (muted)", "720p", "480p", "360p"])
+                                values=["Max (w/ audio)", "1080p (muted)", "720p", "480p", "360p"])
 resolution_combo.pack()
 
 # Download button
@@ -136,16 +196,20 @@ download_button = ttk.Button(root, text="Download", command=download)
 download_button.pack(pady=5)
 
 # Result label
-result_label = ttk.Label(root, text="",wraplength=WINDOW_WIDTH, style="Red.TLabel")
+result_label = ttk.Label(root, text="", wraplength=WINDOW_WIDTH, style="Red.TLabel")
 result_label.pack()
 
 # Initialize resolution combo box state
 resolution_combo.configure(state="readonly")
 
+# Change download folder button
+folder_photo_image = tk.PhotoImage(file=FOLDER_IMAGE_PATH).subsample(FOLDER_IMAGE_SIZE[0], FOLDER_IMAGE_SIZE[1])
+folder_button = ttk.Button(root, image=folder_photo_image, command=change_download_folder)
+folder_button.place(relx=0.98, rely=0.98, anchor="se")  # place at the right bottom of the window
+
 if __name__ == "__main__":
     root.mainloop()
 
-# TODO: make the download path configurable in the gui
 """note: When getting pytube.exceptions.RegetMatchError when getting streams from a YouTube object, temp. workaround is 
 on 'https://github.com/pytube/pytube/issues/1678' from 'mrmechanik':
 The first regex in the function_patterns (cipher.py -> get_throttling_function_name -> function_patterns) does not have a capture group for the method name so I added one:
